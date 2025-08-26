7e515e4b3f3a5b57bda4210d61c70fbab26a5fe1
pohnpimon lakwiroj <pohnpimon_l@muangthai.co.th>
2025-08-27 00:55:18 +0700

    add pom dependency

---
+++ todolist-backend/src/test/java/com/pohnpimon/todolist_backend/TodolistBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pohnpimon.todolist_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
